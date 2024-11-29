bb4a0cfdd34e64acfd8fda55c99a39b91fda6c74
Chris Fletcher <chris.fletcher@evergreen-life.co.uk>
2024-11-29 21:20:53 +0000

    Update suspected invalid lazy.nvim

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -41,7 +41,7 @@ require('lazy').setup({
 				"zipPlugin"
 			}
 		}
-	},
+	}},
 	{"craftzdog/solarized-osaka.nvim", lazy = false, priority = 1000, opts = {} },
 	{ 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
 	-- file tree
