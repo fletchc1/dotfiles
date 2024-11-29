ade28dcd759dfed3a65cbc1fc0e1bcd27e848ea7
Chris Fletcher <chris.fletcher@evergreen-life.co.uk>
2024-11-29 21:50:31 +0000

    Startimg again with lazy.lua, clearly got an error somewhere

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -1,11 +1,8 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -16,11 +13,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup(
-	{"craftzdog/solarized-osaka.nvim", lazy = false, priority = 1000, opts = {} },
+require('lazy').setup({
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{ 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
 	-- file tree
 	{
@@ -35,9 +31,8 @@ require('lazy').setup(
 	  end,
 	},
 	-- bufferline
-	{
-		'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'
-	},
+	{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+
 	-- Markdown preview
 	{
 	    "iamcco/markdown-preview.nvim",
@@ -45,6 +40,7 @@ require('lazy').setup(
 	    ft = { "markdown" },
 	    build = function() vim.fn["mkdp#util#install"]() end,
 	},
+
 	-- Comment toggle
 	{
 		"terrortylor/nvim-comment",
@@ -52,6 +48,7 @@ require('lazy').setup(
 			require("nvim_comment").setup({ create_mappings = false })
 		end
 	},
+	
 	-- LSP setup
 	{
 		  {
