83e43f6f85310ced50d997ac57afccc127bc5001
Chris Fletcher <chris.fletcher@evergreen-life.co.uk>
2026-03-17 15:19:25 +0000

    add script for generating redshift credentials

---
+++ scripts/login_redshift.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# login_redshift.sh
+#
+# Description:
+#   Retrieves Redshift cluster connection information using AWS CLI.
+#
+# Usage:
+#   ./login_redshift.sh <cluster-identifier> <aws-region>
+#
+# Arguments:
+#   cluster-identifier   The identifier of the Redshift cluster.
+#   aws-region           The AWS region where the cluster is located.
+#   db-user              The redshift username to retrieve temporary credentials for.
+#
+# Output:
+#   Prints a JSON object containing the host, port, and dbname of the cluster.
+#
+# Requirements:
+#   - AWS CLI must be installed and configured.
+#   - jq must be installed (if you plan to parse the output).
+#   - must have a means of generating AWS credentials that is called before this script, e.g. aws-vault
+#
+# Example:
+#   ./login_redshift.sh my-cluster us-west-2
+#
+#
+CLUSTER_INFO=$(aws redshift describe-clusters --cluster-identifier $1 --region $2 --query 'Clusters[0].{host: Endpoint.Address, port: Endpoint.Port, dbname: DBName}')
+TEMP_CREDENTIALS=$(aws redshift get-cluster-credentials --cluster-identifier $1 --region $2 --db-user $3 --query '{username: DbUser, password: DbPassword}')
+
+CONNECTION_DATA=$(jq --slurp 'add' <(echo $CLUSTER_INFO) <(echo $TEMP_CREDENTIALS))
+
+echo "$CONNECTION_DATA"
